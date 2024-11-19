ae1a29aa7fb000e821aefe90a5e18dc8322b6c29
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-19 10:13:50 +0100

    Clear screen on most OS

--- main.go
+++ main.go
@@ -4,31 +4,58 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 )
 
+const (
+	helpCommand = "help"
+	exitCommand = "exit"
+)
+
 func main() {
+	clearScreen()
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Welcome to the terminal!")
 	fmt.Println("Type 'help' to see the available commands.")
 
 	for {
 		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		switch input {
-		case "help":
-			fmt.Println("Available commands:")
-			fmt.Println("help - Show this help message")
-			fmt.Println("exit - Exit the game")
-
-		case "exit":
-			fmt.Println("Goodbye")
-			return
-
-		default:
-			fmt.Println("Unknown commands:", input)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
 		}
+		input = strings.TrimSpace(input)
+		handleCommand(input)
 	}
+}
+
+func handleCommand(input string) {
+	switch input {
+	case helpCommand:
+		fmt.Println("Available commands:")
+		fmt.Println("help - Show this help message")
+		fmt.Println("exit - Exit the game")
+
+	case exitCommand:
+		fmt.Println("Goodbye")
+		os.Exit(0)
+
+	default:
+		fmt.Println("Unknown command:", input)
+	}
+}
+
+func clearScreen() {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
